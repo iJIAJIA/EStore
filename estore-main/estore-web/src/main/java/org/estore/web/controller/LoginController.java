4604c11cca87af0f9122d455da6706c4d379fba8
iJIAJIA <kuaicanbukuai@gmail.com>
2016-05-21 20:47:16 +0800

    实现用户登录验证功能

--- estore-main/estore-web/src/main/java/org/estore/web/controller/LoginController.java
+++ estore-main/estore-web/src/main/java/org/estore/web/controller/LoginController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.servlet.http.HttpSession;
 
+import org.estore.common.exception.ProcessException;
 import org.estore.common.secure.IdentityCodeUtils;
 import org.estore.dto.UserSimpleInfo;
 import org.estore.service.UserService;
@@ -12,18 +13,14 @@ import org.estore.web.common.response.LoginResponseCode;
 import org.estore.web.model.IdentityCode;
 import org.estore.web.model.ReturnJsonResult;
 import org.estore.web.model.user.UserLoginInfo;
-import org.estore.web.model.user.UserRegisterInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 public class LoginController {
@@ -33,11 +30,9 @@ public class LoginController {
 	
 	private static final Long IC_USERFUL_TIME = 1000*60*15L; //验证码有效期,15分钟
 	
+	@Autowired
 	private UserService userService;
 	
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
 	@RequestMapping(value="login.do",method=RequestMethod.POST)
 	@ResponseBody
 	public ReturnJsonResult doLogin(@RequestBody UserLoginInfo loginInfo,
@@ -49,17 +44,31 @@ public class LoginController {
 		logger.info("Before removed IdentityCode: {}",iCode);
 		session.removeAttribute(LOGIN_IDENTITYCODE_NAME);
 		logger.info("After removed IdentityCode: {}",iCode);
+		ReturnJsonResult result = null;
 		if(!checkIdentityCode(loginInfo.getIdentifyingCode(),iCode)){
 //			TODO 发送验证码错误响应码
+			result = generateJsonResult(LoginResponseCode.LOGIN_WRONG_IDENTITY_CODE);
 		}
 //		2,获取登录用户信息
-//		UserSimpleInfo userSimpleInfo = userService.findLoginUser(loginInfo.getUserName(), 
-//				loginInfo.getPassword());
-//		logger.debug("userSimpleInfo: {}",userSimpleInfo);
+		UserSimpleInfo userSimpleInfo = null;
+		try {
+			userSimpleInfo = userService.findLoginUser(loginInfo.getUserName(), loginInfo.getPassword());
+		} catch (ProcessException e) {
+			logger.error("验证用户登录信息异常",e.getMessage());
+			result = generateJsonResult(LoginResponseCode.LOGIN_ERROR);
+			return result;
+		}
+		logger.debug("userSimpleInfo: {}",userSimpleInfo);
 //		3.返回信息
-//		ReturnJsonResult result = generateReturnCode4LoginUsr(userSimpleInfo);
-//		return result;
-		return new ReturnJsonResult();
+		if(userSimpleInfo == null){
+			logger.info("User's Login Info is null");
+			result = generateJsonResult(LoginResponseCode.LOGIN_WRONG_NAME_OR_PASSWORD);
+		}else{
+			result = generateJsonResult(LoginResponseCode.LOGIN_SUCCESS);
+			result.setReturnObject(userSimpleInfo);
+		}
+		logger.debug("ReturnJsonResult: {}",result);
+		return result;
 	}
 	
 	@RequestMapping(value="login.index")
@@ -90,8 +99,7 @@ public class LoginController {
 		session.setAttribute(LOGIN_IDENTITYCODE_NAME, iCode);
 		
 //		生成响应信息
-		ReturnJsonResult jsonResult = new ReturnJsonResult();
-		setResponseCode(jsonResult, LoginResponseCode.AUTHCODE_SUCCESS);
+		ReturnJsonResult jsonResult = generateJsonResult(LoginResponseCode.AUTHCODE_SUCCESS);
 		jsonResult.setReturnObject(bImage);
 		
 		return jsonResult;
@@ -102,31 +110,23 @@ public class LoginController {
 	 * @return
 	 */
 	private boolean checkIdentityCode(String code,IdentityCode iCode){
-		logger.debug("IdentityCode: {} || DeadTime: {}",iCode.getCode(),iCode.getDeadTime());
+//		logger.debug("IdentityCode: {} || DeadTime: {}",iCode.getCode(),iCode.getDeadTime());
 		
 		return true;
 	}
 	
 	/**
-	 * 根据用户信息生成返回返回码
-	 * @param userInfo
+	 * 设置响应编码和信息
+	 * @param respCode
 	 * @return
 	 */
-	private ReturnJsonResult generateReturnCode4LoginUsr(UserSimpleInfo userInfo){
+	private ReturnJsonResult generateJsonResult(LoginResponseCode respCode){
+		if(respCode == null){
+			return null;
+		}
 		ReturnJsonResult result = new ReturnJsonResult();
-		result.setReturnCode("0000");
-		result.setReturnMsg("登录成功!");		
-		
+		result.setReturnCode(respCode.getCode());
+		result.setReturnMsg(respCode.getMessage());
 		return result;
-	}
-	
-	/**
-	 * 将响应码和响应信息写入ReturnJsonResult中
-	 * @param jsonResult
-	 * @param respCode
-	 */
-	private void setResponseCode(ReturnJsonResult jsonResult,LoginResponseCode respCode){
-		jsonResult.setReturnCode(respCode.getCode());
-		jsonResult.setReturnMsg(respCode.getMessage());
 	}
 }
